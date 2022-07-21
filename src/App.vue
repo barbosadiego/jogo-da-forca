356332430baf7fb447d390401a346b55fa26ca78
diegobarbosa <diego.gutemberg@hotmail.com>
2022-07-21 09:53:11 -0300

    inicio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="game-container">
+      <h1>Jogo da Forca</h1>
+      <Formulario />
+      <Formulario />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formulario from '@/components/Formulario.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Formulario,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+:root {
+  --color-background: #2b343e;
+  --color-background-button: #2980b9;
+  --color-text-dark: #2c3e50;
+  --color-text-light: #ecf0f1;
+  --color-text-title: #1abc9c;
+  --color-text-success: #2ecc71;
+  --color-text-error: #c0392b;
+}
+body {
+  background-color: var(--color-background);
+  font-family: Arial, Helvetica, sans-serif;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+h1 {
+  color: var(--color-text-title);
+}
+.game-container{
+  display: grid;
+  gap: 30px;
 }
 </style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="formulario">
+    <p>Defina a palavra</p>
+    <input type="text">
+    <button>Pŕoximo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheFormulario',
+
+}
+</script>
+
+<style scoped>
+.formulario p{
+  color: var(--color-text-light);
+}
+</style>
